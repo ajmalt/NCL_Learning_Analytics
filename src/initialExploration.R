85ef6987d61ffd1938898401abfa28e454d84eb8
Alex Malt (PGT) <a4066567@newcastle.ac.uk>
2019-11-19 13:18:03 +0000

    Commit 5: first exploration into Archetype data; start to do list

---
+++ src/initialExploration.R
@@ -0,0 +1,38 @@
+#-----Initial data exploration
+load.project() #Load project and data
+
+
+#archetype survey responses
+summary(cyber.security.1.archetype.survey.responses)
+str(cyber.security.1.archetype.survey.responses)
+
+summary(cyber.security.2.archetype.survey.responses)
+str(cyber.security.2.archetype.survey.responses)
+
+summary(cyber.security.3.archetype.survey.responses)
+str(cyber.security.3.archetype.survey.responses)
+head(cyber.security.3.archetype.survey.responses)
+levels(cyber.security.3.archetype.survey.responses$archetype)
+cyber.security.3.archetype.survey.responses$id
+
+summary(cyber.security.4.archetype.survey.responses)
+str(cyber.security.4.archetype.survey.responses)
+levels(cyber.security.4.archetype.survey.responses$archetype)
+cyber.security.4.archetype.survey.responses$id
+
+summary(cyber.security.5.archetype.survey.responses)
+str(cyber.security.5.archetype.survey.responses)
+levels(cyber.security.5.archetype.survey.responses$archetype)
+
+summary(cyber.security.6.archetype.survey.responses)
+str(cyber.security.6.archetype.survey.responses)
+levels(cyber.security.6.archetype.survey.responses$archetype)
+
+summary(cyber.security.7.archetype.survey.responses)
+str(cyber.security.7.archetype.survey.responses)
+levels(cyber.security.7.archetype.survey.responses$archetype)
+
+
+#?
+
+
