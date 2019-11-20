51b2710d9d7497edeb0906e172f3f5fe472d0045
Alex Malt (PGT) <a4066567@newcastle.ac.uk>
2019-11-20 17:11:53 +0000

    Commit 15: end of day back up

---
+++ munge/01-F.R
@@ -0,0 +1,32 @@
+# Preprocessing script 01-F
+# Weekly sentiment data
+#str(cyber.security.7.weekly.sentiment.survey.responses) # check structure and identify date columns
+#summary(cyber.security.7.weekly.sentiment.survey.responses) # gain familiarity with general properties of the data
+
+
+#Remove empty datasets
+rm(cyber.security.1.weekly.sentiment.survey.responses, cyber.security.2.weekly.sentiment.survey.responses, 
+   cyber.security.3.weekly.sentiment.survey.responses, cyber.security.4.weekly.sentiment.survey.responses)
+
+
+#merge datasets
+sentimentData <- rbind(cyber.security.5.weekly.sentiment.survey.responses, 
+                       cyber.security.6.weekly.sentiment.survey.responses, 
+                       cyber.security.7.weekly.sentiment.survey.responses)
+
+
+#convert experience_rating and week_number to factors
+sentimentData$experience_rating <- factor(sentimentData$experience_rating)
+sentimentData$week_number <- factor(sentimentData$week_number)
+#levels(sentimentData$week_number) #checking factor levels
+
+
+#create responded_date column
+sentimentData$responded_date <- as.Date(as.character(sentimentData$responded_at))
+#str(sentimentData) # checking column types
+
+
+#remove redundant datasets
+rm(cyber.security.5.weekly.sentiment.survey.responses, 
+   cyber.security.6.weekly.sentiment.survey.responses, 
+   cyber.security.7.weekly.sentiment.survey.responses)
---
+++ munge/01-G.R
@@ -0,0 +1,5 @@
+# Preprocessing script 01-G
+# team member data
+#str(cyber.security.7.team.members) # check structure and identify date columns
+#summary(cyber.security.2.team.members) # gain familiarity with general properties of the data
+
